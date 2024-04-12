37e78bf64f4c18cd9119600b04436a1b38e3268b
tuannt1 <tuannt1@paditech.com>
2024-04-12 17:48:56 +0700

    change contract loyal NFT upgradeable

--- contracts/LoyalNFT.sol
+++ contracts/LoyalNFT.sol
@@ -1,24 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
-import "erc721a/contracts/ERC721A.sol";
+import "erc721a-upgradeable/contracts/ERC721AUpgradeable.sol";
+import "hardhat/console.sol";
 
-contract LoyalNFT is ERC721A, Ownable, ReentrancyGuard {
+contract LoyalNFT is
+    ERC721AUpgradeable,
+    Initializable,
+    UUPSUpgradeable,
+    OwnableUpgradeable,
+    ReentrancyGuardUpgradeable
+{
     IERC20Metadata token;
 
     uint256 public constant MAX_SUPPLY = 14000;
     uint256 private constant MAX_BUY = 11000;
     uint256 private constant RANGE_CHANGE_PRICE = 200;
-    uint256 private basePrice = 1000;
+    uint256 private constant basePrice = 1000;
     address private devAddress;
+    address public superAdmin;
 
     uint256 public totalSold;
     uint256 totalDropTimes;
     string tokenBaseURI;
+
+    struct Voucher {
+        uint256 startTime;
+        uint256 endTime;
+        uint256 discountPercent;
+    }
+    Voucher public voucher;
 
     event Buyed(address buyer, uint256 price);
     event Withdraw(address owner, uint256 amount);
@@ -33,21 +48,33 @@ contract LoyalNFT is ERC721A, Ownable, ReentrancyGuard {
     //                     1400 -1599 => 1020$
     //                     ....
     mapping(uint256 => uint256) rangePriceToPrice; // (totalSold) /200 => price
+    mapping(address => bool) public whitelist;
 
-    constructor(
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(
         address _token,
         string memory _initBaseURI,
         address _devAddress,
         uint256[] memory prices,
+        string memory name,
+        string memory symbol,
         address _administratorAddress
-    ) ERC721A("LoyalNFT", "LNFT") {
+    ) public initializerERC721A initializer {
+        __Ownable_init();
+        __UUPSUpgradeable_init();
+        __ReentrancyGuard_init();
+        __ERC721A_init(name, symbol);
         token = IERC20Metadata(_token);
         tokenBaseURI = _initBaseURI;
         devAddress = _devAddress;
         for (uint256 i = 0; i < prices.length; i++) {
             rangePriceToPrice[i + 5] = prices[i];
         }
-        transferOwnership(_administratorAddress);
+        superAdmin = _administratorAddress;
     }
 
     /**
@@ -75,6 +102,15 @@ contract LoyalNFT is ERC721A, Ownable, ReentrancyGuard {
             );
             buyPrice = _calculatePrice();
         }
+
+        uint256 discountPercent = 0;
+        if (
+            block.timestamp >= voucher.startTime &&
+            block.timestamp <= voucher.endTime
+        ) {
+            discountPercent = voucher.discountPercent;
+        }
+        buyPrice = buyPrice - (buyPrice * discountPercent) / 100;
 
         require(
             token.transferFrom(
@@ -107,7 +143,7 @@ contract LoyalNFT is ERC721A, Ownable, ReentrancyGuard {
     /**
      * @notice Function to withdraw the token for the owner
      */
-    function withdraw() external onlyOwner {
+    function withdraw() external onlySuperAdmin {
         uint256 balance = token.balanceOf(address(this));
         require(balance > 0, "No tokens to withdraw");
 
@@ -170,6 +206,9 @@ contract LoyalNFT is ERC721A, Ownable, ReentrancyGuard {
         return sellPrice;
     }
 
+    function getNumberDropableNFT() public view returns (uint256) {
+        return (totalSold / 100 - totalDropTimes) * 10;
+    }
     /**
      * @notice Function to get current price of NFT.
      */
@@ -180,4 +219,66 @@ contract LoyalNFT is ERC721A, Ownable, ReentrancyGuard {
             return rangePriceToPrice[totalSold / RANGE_CHANGE_PRICE];
         }
     }
+
+    function setVoucher(
+        uint256 _startTime,
+        uint256 _endTime,
+        uint256 _percent
+    ) external onlyOwner {
+        require(_startTime < _endTime, "Invalid time");
+        require(_percent > 0 && _percent < 100, "Invalid percent");
+
+        voucher.startTime = _startTime;
+        voucher.endTime = _endTime;
+        voucher.discountPercent = _percent;
+    }
+
+    function _beforeTokenTransfers(
+        address from,
+        address to,
+        uint256 tokenId,
+        uint256
+    ) internal virtual override {
+        require(
+            whitelist[from] == true ||
+                whitelist[to] == true ||
+                from == address(0),
+            "Transfer NFT failed"
+        );
+        super._beforeTokenTransfers(from, to, tokenId, 1);
+    }
+    /**
+     * @notice Add to whitelist
+     */
+    function addToWhitelist(
+        address[] calldata toAddAddresses
+    ) external onlyOwner {
+        for (uint i = 0; i < toAddAddresses.length; i++) {
+            whitelist[toAddAddresses[i]] = true;
+        }
+    }
+
+    /**
+     * @notice Remove from whitelist
+     */
+    function removeFromWhitelist(
+        address[] calldata toRemoveAddresses
+    ) external onlyOwner {
+        for (uint i = 0; i < toRemoveAddresses.length; i++) {
+            delete whitelist[toRemoveAddresses[i]];
+        }
+    }
+
+    function changeOwner(address newOwner) external onlySuperAdmin {
+        _transferOwnership(newOwner);
+    }
+
+    modifier onlySuperAdmin() {
+        require(msg.sender == superAdmin, "Not super admin address");
+        _;
+    }
+
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal override onlyOwner {}
 }
